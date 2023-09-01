07f63b66594ce0c44ac5ef29b294d0a34e837ca4
luksazvdoo <140004341+luksazvdoo@users.noreply.github.com>
2023-09-01 15:18:05 -0300

    codigo da calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,11 @@
 def add(a, b):
-    if a>10:
-        a = a+8
     return a + b
 def multiplicacao(a, b):
     return a * b
+def divisao(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero não é permitida.")
+    return a / b
 def subtracao(a,b):
     return a - b
 def fatorial(x):
