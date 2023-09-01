ab2910ab2f4acd3b76bbe26c349da7847d460458
luksazvdoo <140004341+luksazvdoo@users.noreply.github.com>
2023-09-01 15:12:37 -0300

    Update test_calculadora.py

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,6 +1,7 @@
 import calculadora
 
 assert calculadora.add(3,2)==5
+assert calculadora.multiplicacao(3,2)==6
+assert calculadora.divisao(3,3)==1
 assert calculadora.subtracao(3,2)==1
-#assert calculadora.multiplicacao(3,2)==6
 assert calculadora.fatorial(3)==6
